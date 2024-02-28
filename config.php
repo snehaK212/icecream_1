43d2069d94018120b198ea6d939285a297ea02ce
Sneha Balaji Karwande <114145431+snehaK212@users.noreply.github.com>
2024-02-28 12:45:14 +0530

    update

--- config.php
+++ config.php
@@ -1,4 +1,8 @@
 <?php
 
 $conn=mysqli_connect('icedb.mysql.database.azure.com','admin1','@Sneha123','icedb') or die('connection failed');
+<<<<<<< HEAD
+
+=======
+>>>>>>> 6bb112acdeb24a954b88ecfa04d8c1fe7f981dae
 ?>
